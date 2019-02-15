21b9f411080716654ec97b79e032a2db95e5403e
fad1412 <47650096+fad1412@users.noreply.github.com>
2019-02-15 09:02:32 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'AR/mEPCWEHjTgehJg+nVhqHH1tJYFqJsALpuKDorswwCIq4LtAek9ym9QSwXJoMO1IuhZlHnnxBSO7iMI+BhN9wpLJxk6/NP7YhQPqkqerIoG+J+6mmDXMifGC512Yxw7V5ADVASfsW4aTU6qUOg4wdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
