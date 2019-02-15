2200f120611925442cdcf9be3ee47d82ed143b8b
fad1412 <47650096+fad1412@users.noreply.github.com>
2019-02-15 09:49:17 +0700

    Update test01.php

--- test01.php
+++ test01.php
@@ -2,7 +2,7 @@
 require "vendor/autoload.php";
 $access_token = 'AR/mEPCWEHjTgehJg+nVhqHH1tJYFqJsALpuKDorswwCIq4LtAek9ym9QSwXJoMO1IuhZlHnnxBSO7iMI+BhN9wpLJxk6/NP7YhQPqkqerIoG+J+6mmDXMifGC512Yxw7V5ADVASfsW4aTU6qUOg4wdB04t89/1O/w1cDnyilFU=';
 $channelSecret = '66766a0d5943074c5579539cce64a416';
-$idPush = 'Uf8d583d3729b126c1755461ebba76dd';
+$idPush = 'Uf8d583d3729b126c1755461eb6a76dd';
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
 $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello world');
